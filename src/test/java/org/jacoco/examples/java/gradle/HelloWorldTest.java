d3f95b627d8db7da803e57ea4943e7c49fa20844
Jesper Leerberg Madsen <jesper.leerberg@gmail.com>
2019-03-11 22:21:37 +0100

    initial setup

---
+++ src/test/java/org/jacoco/examples/java/gradle/HelloWorldTest.java
@@ -0,0 +1,28 @@
+package org.jacoco.examples.java.gradle;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloWorldTest {
+
+	private HelloWorld subject;
+
+	@Before
+	public void setup() {
+		subject = new HelloWorld();
+	}
+	
+	@Test
+	public void testGetMessage() {
+		assertEquals("Hello World!", subject.getMessage(false));
+	}
+
+
+	@Test
+	public void testGetMessage3() {
+		assertEquals("Hello Universe!", subject.getMessage(true));
+	}
+
+}
