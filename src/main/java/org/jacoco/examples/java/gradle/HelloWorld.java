4bbd612590c5e48105d732b1f84a6a5fb42f2f69
Jesper Leerberg Madsen <jesper.leerberg@gmail.com>
2019-10-10 13:14:23 +0200

    try introducing sonar bugs

--- src/main/java/org/jacoco/examples/java/gradle/HelloWorld.java
+++ src/main/java/org/jacoco/examples/java/gradle/HelloWorld.java
@@ -10,4 +10,14 @@ public class HelloWorld {
 		}
 	}
 
+	public void speech() {
+		String speech = "Now is the time for all good people to come to the aid of their country.";
+
+		String substr1 = speech.substring(-1, speech.length());  // Noncompliant; start and end values both bad
+		String substr2 = speech.substring(speech.length(), 0); // Noncompliant, start value must be < end value
+		char ch = speech.charAt(speech.length());  // Noncompliant
+
+		System.out.println(substr2);
+	}
+
 }
